42d39dbdb9eab48156bdffd9b45481b00c51300c
CodeRaiser <code.geeks1@gmail.com>
2020-12-25 22:25:13 +0200

    Implement Report class

---
+++ app/Slots/Report.php
@@ -0,0 +1,129 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Slots;
+
+class Report
+{
+    /**
+     * @var array{
+     *     mysterySymbols: array{
+     *         mysteryTile: int,
+     *         toTile: int,
+     *         tiles?: array{
+     *             reel?: int,
+     *             index?: int,
+     *             sequence?: array{
+     *                 int,
+     *                 int,
+     *                 int
+     *             }
+     *         }
+     *     },
+     *     reelsBuffer: array<int, array<int>>
+     * }
+     */
+    private array $reelsBuffer;
+
+    /**
+     * @var array{
+     *     updatedReelsBuffer: array<int, array<int>>,
+     *     matchedLines: array{'realized_lines': array<int, array{coords?: array{reel?: int, tile?: int}, value?: int}>}
+     * }
+     */
+    private array $analyzeReport;
+
+    /**
+     * @param array{
+     *     mysterySymbols: array{
+     *         mysteryTile: int,
+     *         toTile: int,
+     *         tiles?: array{
+     *             reel?: int,
+     *             index?: int,
+     *             sequence?: array{
+     *                 int,
+     *                 int,
+     *                 int
+     *             }
+     *         }
+     *     },
+     *     reelsBuffer: array<int, array<int>>
+     * } $reelsBuffer
+     */
+    public function setReelsBuffer(array $reelsBuffer): void
+    {
+        $this->reelsBuffer = $reelsBuffer;
+    }
+
+    /**
+     * @param array{
+     *     updatedReelsBuffer: array<int, array<int>>,
+     *     matchedLines: array{'realized_lines': array<int, array{coords?: array{reel?: int, tile?: int}, value?: int}>}
+     * } $analyzeReport
+     */
+    public function setAnalyzeReport(array $analyzeReport): void
+    {
+        $this->analyzeReport = $analyzeReport;
+    }
+
+    /**
+     * @return array{
+     *     mysterySymbols: array{
+     *         mysteryTile: int,
+     *         toTile: int,
+     *         tiles?: array{
+     *             reel?: int,
+     *             index?: int,
+     *             sequence?: array{
+     *                 int,
+     *                 int,
+     *                 int
+     *             }
+     *         }
+     *     },
+     *     reelsBuffer: array<int, array<int>>
+     * }
+     */
+    public function getReelsBuffer(): array
+    {
+        return $this->reelsBuffer;
+    }
+
+    /**
+     * @return array{
+     *     updatedReelsBuffer: array<int, array<int>>,
+     *     matchedLines: array{'realized_lines': array<int, array{coords?: array{reel?: int, tile?: int}, value?: int}>}
+     * }
+     */
+    public function getAnalyzeReport(): array
+    {
+        return $this->analyzeReport;
+    }
+
+    /**
+     * @return array{
+     *     mysterySymbols: array{
+     *         mysteryTile: int,
+     *         toTile: int,
+     *         tiles?: array{
+     *             reel?: int,
+     *             index?: int,
+     *             sequence?: array{
+     *                 int,
+     *                 int,
+     *                 int
+     *             }
+     *         }
+     *     },
+     *     reelsBuffer: array<int, array<int>>,
+     *     updatedReelsBuffer: array<int, array<int>>,
+     *     matchedLines: array{'realized_lines': array<int, array{coords?: array{reel?: int, tile?: int}, value?: int}>}
+     * }
+     */
+    public function getReport(): array
+    {
+        return $this->reelsBuffer + $this->analyzeReport;
+    }
+
+}
